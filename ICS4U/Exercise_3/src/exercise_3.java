d6a51c315522f6011efd1dfdc33de187553d1f58
JENNY WONG <s331381749@ocdsb>
2018-09-20 14:36:27 -0400

    yo

--- ICS4U/Exercise_3/src/exercise_3.java
+++ ICS4U/Exercise_3/src/exercise_3.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 
 public class exercise_3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		 answer1;
+		String age;
+		String address;
+		
+		Scanner scan1 = new Scanner(System.in);
+				
+		System.out.print("What is 5+5?:");
+		answer1 = scan1.nextLine(); //READS STRINGS ONLY
+		if(answer1 == 10) {
+			
+		}
+		
+		
+		System.out.print("enter your age: ");
+		age = scan1.nextLine();
+		System.out.print("enter your address: ");
+		address = scan1.nextLine();
+		
+		//OUTPUT THE INFO
+		System.out.print("Hi " + name + "! I hear that you live at " + address + "! i also heard you are " + age + " years old.");
+		
+		scan1.close();
+		
+		
+		
 	}
 
 }
