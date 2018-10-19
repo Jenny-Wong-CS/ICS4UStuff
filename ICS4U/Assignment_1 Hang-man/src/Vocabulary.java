2c03c6b00df5f6609f51e8de90760ccd7def7ad0
JENNY WONG <s331381749@ocdsb>
2018-10-19 13:13:10 -0400

    why

--- ICS4U/Assignment_1 Hang-man/src/Vocabulary.java
+++ ICS4U/Assignment_1 Hang-man/src/Vocabulary.java
@@ -19,31 +19,60 @@ public class Vocabulary {
 		}
 		Words.closeInputFile();
 
-		
 		String[] Vocabulary =  new String[numLines];
 
 		Words.openInputFile("Vocabulary_Library.txt");
-		for(int i = 0; i < numLines; i++)
+		for(int i = 0; i < numLines; i++) {
 			Vocabulary[i] = Words.readLine();
-		Words.closeInputFile();
-
-
-        Random rand = new Random();
-        //String Guess;
-
-        for(int i = rand.nextInt((50 - 1) +1) ; i < numLines;)
-
-        System.out.println("the chosen word is: " + Vocabulary[i] );
+			Words.closeInputFile();
+		}
 
 
 
-        Scanner scan1 = new Scanner(System.in);
-        char Guess;
-        System.out.println("Take a guess:");
-        Guess = scan1.nextLine();
+		Random rand = new Random();
+
+		String Word;
+		String Guess;
 
 
-        if ()
+        for(int i = rand.nextInt((50 - 1) +1); i < numLines;){
+			Word = Vocabulary[i];
+		}
+		//Char[] Guess = Word.toCharArray();
+
+		if (Word.contains(Guess) ) {
+			System.out.println("The letter" + Guess + "is in the word");
+		}
+
+
+		for (int w =0; w < Word.length(); w++)
+			char Guess = Word.charAt(w);
+		if (Guess(Word, Guess))
+			System.out.println(Guess);
+		else {
+			System.out.print("-");
+		}
+
+
+		Scanner Scan1 = new Scanner(System.in);
+
+		System.out.println("Input letter: ");
+		Guess.scan1.nextLine();
+
+
+
+
+        //System.out.println("the chosen word is: " + Vocabulary[i] );
+
+
+
+//		String Guess;
+//        Scanner Scan1 = new Scanner(System.in);
+//        System.out.println("Take a guess:");
+//        Guess = Scan1.nextLine();
+//
+//		String Letter = Guess;
+//        if (Letter.indexOf(Vocabulary) != -1)
 
 
 
