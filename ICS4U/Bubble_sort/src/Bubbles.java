a63f0b95850d6665198688bf62a9309e790983c5
Jenny-Wong-CS <jwong14@ocdsb.ca>
2018-10-29 02:56:12 -0400

    fuk

--- ICS4U/Bubble_sort/src/Bubbles.java
+++ ICS4U/Bubble_sort/src/Bubbles.java
@@ -4,14 +4,30 @@ public class Bubbles {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
+        IO.createOutputFile("List.txt");
+        IO.openInputFile();
+        String line = IO.readLine();
+        int numLines = 0;
+
+        while (line != null) {
+            numLines++;
+            line = IO.readLine();
+        }
+        IO.closeOutputFile();
+
+        int[] Vocabulary = new String[numLines];
+
+
+
+
         boolean Switch;
-
+        IO.openInputFile("List.txt");
         while (Switch = true) {
-            int List = 0;
             int Temp;
+            //int Box;
 
-            for (int i = 0; i < List; i++ ) {
-                if ( ) {
+            for (int i = 0; i < NumLines; i++ ) {
+                if (int Box[i]< Box[i]{
                     System.out.println("The letter " + Guess + "is in the word");
                 } else {
                     System.out.println("Try again!");
@@ -23,6 +39,8 @@ public class Bubbles {
 
 
         }
+        IO.closeInputFile();
+
 
     }
 
