83ddd5e7c0364dc4ac0ea49b596707de6198c710
Jenny Wong <Jwong14@ocdsb.ca>
2018-11-21 12:15:03 -0500

    yike

--- ICS4U/untitled/src/ofcoursenotthatimadetheporjectname.java
+++ ICS4U/untitled/src/ofcoursenotthatimadetheporjectname.java
@@ -20,17 +20,17 @@ public class ofcoursenotthatimadetheporjectname {
         //subtracts all the numbers together, the number is made positive because of "=-"
         int total4 =-(-num1-num2-num3);
 
-        //adds all th inputs together-FILLER
+        //adds all the inputs together-FILLER
         int total6 =  num1+num2+num3;
 
-        //subtracts the inputs together, and makes the difference negative-FILLER
+        //subtracts the inputs together, and makes the difference negative(unless the difference is a negative, in which it becomes positive because of "=-")-FILLER
         int total7 =-(num1-num2-num3);
 
         // assigned the int sum4 to the int sum, everything else in line is filler
         Assignment(total3, total4, 43720, total1, total6, "greawett", 73689, total7);
 
     }
-    //function for the sum-the second int is the sum for the imputed numbers
+    //function for the sum-the second int will be the sum for the imputed numbers
     static void Assignment(int Filler1, int sum, int Filler2, int Filler3, int Filler4, String Filler5, int Filler6, int Filler7)
 
     //prints out the sum
